aa4444ece39c45c27e610b6d85d628ad87b4b882
ilyas mohetna <ilyas.mohetna@hotmail.com>
2024-10-09 20:10:22 +0200

    ajout getters and setters pour la classe cercle

--- App1/Cercle.java
+++ App1/Cercle.java
@@ -1,22 +1,34 @@
 package App1;
 
 public class Cercle {
-    private double rayon;
+    private double radius;
     private final double pi = 3.14;
 
-    public Cercle(double rayon){
-        this.rayon = rayon;
+    public Cercle(double radius){
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getPi() {
+        return this.pi;
     }
 
     public double getPerimeter()
     {
-        double diametre = this.rayon * 2;
+        double diametre = this.radius * 2;
         return diametre * this.pi;
     }
 
     public double getArea()
     {
-        return this.rayon * this.rayon * pi;
+        return this.radius * this.radius * pi;
     }
     
     public static void main(String[] args) {
