2d9dad16bd5cb08f6ba7ea10078cf6dede561898
IlyasMohetna <109673941+IlyasMohetna@users.noreply.github.com>
2024-10-23 13:17:43 +0200

    Enseignent chercheur est un enseignent

--- app/src/main/java/com/pr70/TP/TP2/Universite/Entite/EnseignantChercheur.java
+++ app/src/main/java/com/pr70/TP/TP2/Universite/Entite/EnseignantChercheur.java
@@ -1,9 +1,8 @@
 package com.pr70.TP.TP2.Universite.Entite;
 
-import com.pr70.TP.TP2.Universite.Interface.EstCoursIntervenant;
 import com.pr70.TP.TP2.Universite.Interface.EstRechercheParticipant;
 
-public class EnseignantChercheur extends Personne implements EstCoursIntervenant, EstRechercheParticipant{
+public class EnseignantChercheur extends Enseignant implements EstRechercheParticipant{
     public EnseignantChercheur(String prenom, String nom, String age){
         super(prenom, nom, age);
     }
